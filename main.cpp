cda42be3199802ea24a970e716f3db920feac626
nikita-chizh <nikita1993chizh@gmail.com>
2017-12-10 23:57:11 +0300

    Add hello world wor testing

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include "lib.h"
 #include <spdlog/spdlog.h>
-
 int main(int argc, char* argv[]){
     auto logger = spdlog::stdout_logger_mt("console");
     logger -> info("version {}", version());
+    std::cout<<"hello world";
     return 0;
 }
