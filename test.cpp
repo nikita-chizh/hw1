383b515ca097e45f22f8378ededb236709db1b83
nikita-chizh <nikita1993chizh@gmail.com>
2017-12-11 01:04:15 +0300

    another try to deploy

--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@ BOOST_AUTO_TEST_SUITE( test_suite )
     BOOST_AUTO_TEST_CASE( test_version )
     {
         auto console = spdlog::stdout_logger_st("console");
-        console->info("test version()");
+        console->info("test version1()");
 
         BOOST_CHECK( version() > 0 );
     }
